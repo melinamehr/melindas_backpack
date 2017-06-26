69ef775cd2c32b1fca0fe1160431adafbf20aaec
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-26 13:11:06 -0400

    removed unncessary comments

--- backpack.rb
+++ backpack.rb
@@ -28,7 +28,6 @@ class Backpack
 
     # Ensure gym shoes are added to backpack on Mondays & Thursdays
     if day_of_week == 'monday' || day_of_week == 'thursday'
-      #add gym shoes to items
       @items << 'gym shoes'
     end
 
@@ -36,12 +35,10 @@ class Backpack
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
     elsif false
-      # Used to bring snacks on weekend trips, but now I just buy 'em
       @items << 'snacks'
     end
   end
 
-  # Prints a summary packing list for Melinda's backpack
   def packing_list
     output = []
     output << "Melinda, here's your packing list!"
