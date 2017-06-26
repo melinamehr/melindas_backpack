af5f4ee3ab910364927b1c1e560cfe0a9eb03a90
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-26 13:55:28 -0400

    continued to refactor the prepare method

--- backpack.rb
+++ backpack.rb
@@ -3,6 +3,7 @@ class Backpack
     @attributes = attributes # a hash containing day_of_week and weather keys
     @items = []
     prepare
+    extra_items
   end
 
   def items
@@ -16,26 +17,24 @@ class Backpack
 
   def prepare
     weather = @attributes[:weather]
-    day_of_week = @attributes[:day_of_week]
-
     if weather == 'rainy'
-      standard_clothes
       @items << 'umbrella'
-    else weather == 'cold'
-      standard_clothes
+    elsif weather == 'cold'
       @items << 'jacket'
+    else standard_clothes
     end
+  end
 
-    # Ensure gym shoes are added to backpack on Mondays & Thursdays
+  def extra_items
+    day_of_week = @attributes[:day_of_week]
     if day_of_week == 'monday' || day_of_week == 'thursday'
       @items << 'gym shoes'
     end
 
-    # Bring a packed lunch on all weekdays
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
     end
-end
+  end
 
   def packing_list
     output = []
